2f8992984637f75e4e746024dc9369fc253abf58
EBA <ep.bogdy@gmail.com>
2018-06-12 16:24:12 +0300

    2

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 var bodyParser = require('body-parser');
+const path = require('path');
 
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-app.use(express.static('../frontend'));
+app.use(express.static(path.join(__dirname,'../frontend')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
